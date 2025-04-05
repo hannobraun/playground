0d9019d346ecb316388bff50cd2fcf9e91d7e54f
Hanno Braun <hello@hannobraun.com>
2025-04-05 14:45:30 +0200

    Prepare for follow-on change

--- main.lua
+++ main.lua
@@ -6,9 +6,9 @@ local block_offset = 1
 local block_size_inner = block_size - 2 * block_offset
 
 function love.draw()
-    for i = 0, field_width - 1, 1 do
+    for i = 1, field_width, 1 do
         for j = 1, field_height, 1 do
-            local x = i * block_size
+            local x = (i - 1) * block_size
             local y = block_size * field_height - j * block_size
             
             love.graphics.rectangle(
