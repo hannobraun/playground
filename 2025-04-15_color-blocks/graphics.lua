6d20268abf861bcaa85ec44a28620412d03e980a
Hanno Braun <hello@hannobraun.com>
2025-04-15 14:36:07 +0200

    Refactor

--- 2025-04-15_color-blocks/graphics.lua
+++ 2025-04-15_color-blocks/graphics.lua
@@ -49,12 +49,14 @@ local function draw(field)
 
     for i = 0, field.size_x - 1 do
         for j = 0, field.size_y - 1 do
-            love.graphics.rectangle(
-                "fill",
-                Field:offset() + i * Cell.size + Block:offset(),
-                Field:offset() + j * Cell.size + Block:offset(),
-                Block.size, Block.size
-            )
+            if field[field.index(i, j)] then
+                love.graphics.rectangle(
+                    "fill",
+                    Field:offset() + i * Cell.size + Block:offset(),
+                    Field:offset() + j * Cell.size + Block:offset(),
+                    Block.size, Block.size
+                )
+            end
         end
     end
 end
--- 2025-04-15_color-blocks/main.lua
+++ 2025-04-15_color-blocks/main.lua
@@ -8,6 +8,18 @@ local field = {
     size_y = 16,
 }
 
+function field.index(x, y)
+    return y * field.size_y + x
+end
+
+
+function love.load()
+    for x = 0, field.size_x - 1 do
+        for y = 0, field.size_y - 1 do
+            field[field.index(x, y)] = true
+        end
+    end
+end
 
 function love.keypressed(key)
     if key == "escape" then
