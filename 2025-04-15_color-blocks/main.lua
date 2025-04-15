51c36f81ff24a5e2939278f0f35c9645004de802
Hanno Braun <hello@hannobraun.com>
2025-04-15 12:51:09 +0200

    Prepare for follow-on change

--- 2025-04-15_color-blocks/main.lua
+++ 2025-04-15_color-blocks/main.lua
@@ -2,6 +2,7 @@ local lick = require "lick"
 
 function love.draw()
     local field_size_x = 10
+    local field_size_y = 16
 
     local cell_size = 32
 
@@ -9,7 +10,7 @@ function love.draw()
     local block_offset = (cell_size - block_size) / 2
 
     for i = 1, field_size_x, 1 do
-        for j = 1, 16, 1 do
+        for j = 1, field_size_y, 1 do
             love.graphics.rectangle(
                 "fill",
                 i * cell_size + block_offset, j * cell_size + block_offset,
