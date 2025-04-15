6d7f8a2f069e0e432026e33746a50b0a29a7fa97
Hanno Braun <hello@hannobraun.com>
2025-04-15 13:36:09 +0200

    Consolidate field size in new `field` object

--- 2025-04-15_color-blocks/main.lua
+++ 2025-04-15_color-blocks/main.lua
@@ -1,7 +1,9 @@
 local lick = require "lick"
 
-local field_size_x = 10
-local field_size_y = 16
+local field = {
+    size_x = 10,
+    size_y = 16,
+}
 
 local field_border = 1
 local field_margin = 1
@@ -15,8 +17,8 @@ function love.draw()
 
     draw_border()
 
-    for i = 0, field_size_x - 1 do
-        for j = 0, field_size_y - 1 do
+    for i = 0, field.size_x - 1 do
+        for j = 0, field.size_y - 1 do
             love.graphics.rectangle(
                 "fill",
                 field_offset + i * cell_size + block_offset,
@@ -28,8 +30,8 @@ function love.draw()
 end
 
 function draw_border()
-    local border_size_x = field_size_x * cell_size + field_offset * 2
-    local border_size_y = field_size_y * cell_size + field_offset * 2
+    local border_size_x = field.size_x * cell_size + field_offset * 2
+    local border_size_y = field.size_y * cell_size + field_offset * 2
 
     local bg_r, bg_g, bg_b, bg_a = love.graphics.getBackgroundColor()
     local fg_r, fg_g, fg_b, fg_a = love.graphics.getColor()
