8e0c188275dd77b8a9a26a4b4f5a88c5ad1d21e2
Hanno Braun <hello@hannobraun.com>
2025-04-15 12:44:14 +0200

    Consolidate redundant constants

--- 2025-04-15_color-blocks/main.lua
+++ 2025-04-15_color-blocks/main.lua
@@ -1,12 +1,14 @@
 local lick = require "lick"
 
+local cell_size = 32;
+
 function love.draw()
     for i = 1, 10, 1 do
         for j = 1, 16, 1 do
             love.graphics.rectangle(
                 "fill",
-                i * 32, j * 32,
-                32, 32
+                i * cell_size, j * cell_size,
+                cell_size, cell_size
             )
         end
     end
