8d943adfb30985ec60d18454594ef4183553fd28
Hanno Braun <hello@hannobraun.com>
2025-04-15 12:46:03 +0200

    Center blocks in cell

--- 2025-04-15_color-blocks/main.lua
+++ 2025-04-15_color-blocks/main.lua
@@ -1,14 +1,16 @@
 local lick = require "lick"
 
 local cell_size = 32;
+
 local block_size = 30;
+local block_offset = (cell_size - block_size) / 2
 
 function love.draw()
     for i = 1, 10, 1 do
         for j = 1, 16, 1 do
             love.graphics.rectangle(
                 "fill",
-                i * cell_size, j * cell_size,
+                i * cell_size + block_offset, j * cell_size + block_offset,
                 block_size, block_size
             )
         end
