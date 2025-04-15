668da6253173b2bd70b566effd731f6d3d009c33
Hanno Braun <hello@hannobraun.com>
2025-04-15 12:42:25 +0200

    Update formatting

--- 2025-04-15_color-blocks/main.lua
+++ 2025-04-15_color-blocks/main.lua
@@ -3,7 +3,11 @@ local lick = require "lick"
 function love.draw()
     for i = 1, 10, 1 do
         for j = 1, 16, 1 do
-            love.graphics.rectangle("fill", i * 32, j * 32, 32, 32)
+            love.graphics.rectangle(
+                "fill",
+                i * 32, j * 32,
+                32, 32
+            )
         end
     end
 end
