66bc10d08cba73469c608e556d36d13c7e60a23a
Hanno Braun <hello@hannobraun.com>
2025-04-15 13:32:15 +0200

    Refactor

--- 2025-04-15_color-blocks/main.lua
+++ 2025-04-15_color-blocks/main.lua
@@ -4,7 +4,8 @@ local field_size_x = 10
 local field_size_y = 16
 
 local field_border = 1
-local field_margin = 2
+local field_margin = 1
+local field_offset = field_border + field_margin
 
 local cell_size = 32
 
@@ -18,8 +19,8 @@ function love.draw()
         for j = 0, field_size_y - 1 do
             love.graphics.rectangle(
                 "fill",
-                field_margin + i * cell_size + block_offset,
-                field_margin + j * cell_size + block_offset,
+                field_offset + i * cell_size + block_offset,
+                field_offset + j * cell_size + block_offset,
                 block_size, block_size
             )
         end
@@ -27,8 +28,8 @@ function love.draw()
 end
 
 function draw_border()
-    local border_size_x = field_size_x * cell_size + field_margin * 2
-    local border_size_y = field_size_y * cell_size + field_margin * 2
+    local border_size_x = field_size_x * cell_size + field_offset * 2
+    local border_size_y = field_size_y * cell_size + field_offset * 2
 
     local bg_r, bg_g, bg_b, bg_a = love.graphics.getBackgroundColor()
     local fg_r, fg_g, fg_b, fg_a = love.graphics.getColor()
