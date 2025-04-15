34cb5ef70905b47fcf33d189f16753f11dbf8ae4
Hanno Braun <hello@hannobraun.com>
2025-04-15 14:11:58 +0200

    Prepare for follow-on change

--- 2025-04-15_color-blocks/graphics.lua
+++ 2025-04-15_color-blocks/graphics.lua
@@ -25,21 +25,6 @@ function Block:offset()
 end
 
 
-function draw()
-    draw_border()
-
-    for i = 0, Field.size_x - 1 do
-        for j = 0, Field.size_y - 1 do
-            love.graphics.rectangle(
-                "fill",
-                Field:offset() + i * Cell.size + Block:offset(),
-                Field:offset() + j * Cell.size + Block:offset(),
-                Block.size, Block.size
-            )
-        end
-    end
-end
-
 function draw_border()
     local border_size_x = Field.size_x * Cell.size + Field:offset() * 2
     local border_size_y = Field.size_y * Cell.size + Field:offset() * 2
@@ -60,4 +45,19 @@ function draw_border()
         border_size_x - Field.border * 2, border_size_y - Field.border * 2
     )
     love.graphics.setColor(fg_r, fg_g, fg_b, fg_a)
+end
+
+function draw()
+    draw_border()
+
+    for i = 0, Field.size_x - 1 do
+        for j = 0, Field.size_y - 1 do
+            love.graphics.rectangle(
+                "fill",
+                Field:offset() + i * Cell.size + Block:offset(),
+                Field:offset() + j * Cell.size + Block:offset(),
+                Block.size, Block.size
+            )
+        end
+    end
 end
