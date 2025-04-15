d548b5cee23d87b2faa81e4f45e96e0317c2bfe3
Hanno Braun <hello@hannobraun.com>
2025-04-15 14:19:17 +0200

    Quit on escape key

--- 2025-04-15_color-blocks/main.lua
+++ 2025-04-15_color-blocks/main.lua
@@ -2,6 +2,13 @@ local lick = require "lick"
 
 local graphics = require "graphics"
 
+
+function love.keypressed(key)
+    if key == "escape" then
+        love.event.quit()
+    end
+end
+
 function love.draw()
     graphics.draw()
 end
