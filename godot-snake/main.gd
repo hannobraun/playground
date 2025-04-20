d0407f7e88ce2c61cc07523858043030f55bc11d
Hanno Braun <hello@hannobraun.com>
2025-04-20 20:50:40 +0200

    Consolidate redundant values

--- godot-snake/main.gd
+++ godot-snake/main.gd
@@ -1,8 +1,10 @@
 extends Node2D
 
+const TILE_SIZE = 32
+
 func _draw():
 	draw_rect(
-		Rect2(Vector2(0, 0), Vector2(32, 32)),
+		Rect2(Vector2(0, 0), Vector2(TILE_SIZE, TILE_SIZE)),
 		Color(1, 1, 1, 1),
 		true,
 	)
