90675a082e696147abe1ebd638b499efd7ddbd40
Hanno Braun <hello@hannobraun.com>
2025-04-20 20:59:37 +0200

    Store rectangle position in variable

--- godot-snake/main.gd
+++ godot-snake/main.gd
@@ -2,9 +2,11 @@ extends Node2D
 
 const TILE_SIZE = 32
 
+var pos = Vector2(0, 0)
+
 func _draw():
 	draw_rect(
-		Rect2(Vector2(0, 0), Vector2(TILE_SIZE, TILE_SIZE)),
+		Rect2(pos, Vector2(TILE_SIZE, TILE_SIZE)),
 		Color(1, 1, 1, 1),
 		true,
 	)
