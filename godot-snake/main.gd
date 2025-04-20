6d18bc315cbc00525c4946d3102393dd6a553a0d
Hanno Braun <hello@hannobraun.com>
2025-04-20 21:04:43 +0200

    Position square near center of screen

--- godot-snake/main.gd
+++ godot-snake/main.gd
@@ -4,6 +4,11 @@ const TILE_SIZE = Vector2(32, 32)
 
 var pos = Vector2(0, 0)
 
+func _ready():
+	var field_size = get_viewport().get_visible_rect().size / TILE_SIZE
+	pos = field_size / 2
+	print(pos)
+
 func _draw():
 	draw_rect(
 		Rect2(pos * TILE_SIZE, TILE_SIZE),
