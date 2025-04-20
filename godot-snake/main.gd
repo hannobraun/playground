5825a7d5fe7f3cbbee0d05dbbeed483d95ad5e8c
Hanno Braun <hello@hannobraun.com>
2025-04-20 21:05:46 +0200

    Move square every 200ms

--- godot-snake/main.gd
+++ godot-snake/main.gd
@@ -1,8 +1,10 @@
 extends Node2D
 
 const TILE_SIZE = Vector2(32, 32)
+const TICK = 0.2
 
 var pos = Vector2(0, 0)
+var acc = 0
 
 func _ready():
 	var field_size = get_viewport().get_visible_rect().size / TILE_SIZE
@@ -15,5 +17,11 @@ func _draw():
 		true,
 	)
 
-func _process(_delta):
+func _process(delta):
+	acc += delta
+
+	while acc >= TICK:
+		pos += Vector2(1, 0)
+		acc -= TICK
+
 	queue_redraw()
