c7e7a822b9638751513908202d18cb893eb36d8c
Hanno Braun <hello@hannobraun.com>
2025-07-22 20:14:25 +0200

    Copy reference module to `output/`

--- 2025-07-22_stack/src/main.rs
+++ 2025-07-22_stack/src/main.rs
@@ -1,4 +1,4 @@
-use std::process::Command;
+use std::{fs, path::Path, process::Command};
 
 fn main() -> anyhow::Result<()> {
     let status = Command::new("cargo")
@@ -11,6 +11,14 @@ fn main() -> anyhow::Result<()> {
     if !status.success() {
         anyhow::bail!("Failed to build `wasm-reference`.");
     }
+
+    let output = Path::new("output");
+
+    fs::create_dir_all(output)?;
+    fs::copy(
+        "target/wasm32v1-none/release/wasm_reference.wasm",
+        output.join("reference.wasm"),
+    )?;
 
     Ok(())
 }
