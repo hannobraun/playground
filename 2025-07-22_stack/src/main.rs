7fcbe6b3f49e99b7ccecbd370501583814d8f56f
Hanno Braun <hello@hannobraun.com>
2025-07-22 20:10:49 +0200

    Build `wasm-reference` package

--- 2025-07-22_stack/src/main.rs
+++ 2025-07-22_stack/src/main.rs
@@ -1,4 +1,16 @@
+use std::process::Command;
+
 fn main() -> anyhow::Result<()> {
-    println!("Hello, world!");
+    let status = Command::new("cargo")
+        .arg("build")
+        .args(["--package", "wasm-reference"])
+        .args(["--target", "wasm32v1-none"])
+        .arg("--release")
+        .status()?;
+
+    if !status.success() {
+        anyhow::bail!("Failed to build `wasm-reference`.");
+    }
+
     Ok(())
 }
