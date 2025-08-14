dae34fbb822c812c9fc918096d774fd14289af1a
Hanno Braun <hello@hannobraun.com>
2025-08-14 20:52:20 +0200

    Read code from file

--- 2025-08-02_monobloc/src/main.rs
+++ 2025-08-02_monobloc/src/main.rs
@@ -1,4 +1,17 @@
+use std::{fs::File, io::Read};
+
+use anyhow::Context;
+
 fn main() -> anyhow::Result<()> {
-    println!("Hello, world!");
+    let file = "add.mbl";
+
+    let mut code = String::new();
+    File::open(file)
+        .with_context(|| format!("Opening `{file}`"))?
+        .read_to_string(&mut code)
+        .with_context(|| format!("Reading code from `{file}`"))?;
+
+    println!("{code}");
+
     Ok(())
 }
