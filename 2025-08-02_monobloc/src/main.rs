95a21233263730b92d1b9adbf75059bea482f5ed
Hanno Braun <hello@hannobraun.com>
2025-08-15 13:47:05 +0200

    Update name of function parameter

--- 2025-08-02_monobloc/src/main.rs
+++ 2025-08-02_monobloc/src/main.rs
@@ -30,13 +30,13 @@ fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
-fn read_input_code(file: &str) -> anyhow::Result<String> {
+fn read_input_code(path: &str) -> anyhow::Result<String> {
     let mut buf = String::new();
 
-    File::open(file)
-        .with_context(|| format!("Opening `{file}`"))?
+    File::open(path)
+        .with_context(|| format!("Opening `{path}`"))?
         .read_to_string(&mut buf)
-        .with_context(|| format!("Reading code from `{file}`"))?;
+        .with_context(|| format!("Reading code from `{path}`"))?;
 
     Ok(buf)
 }
