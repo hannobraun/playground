d98c0ab333bdad54acf5ea8791c667fc2eac539a
Hanno Braun <hello@hannobraun.com>
2025-08-15 13:43:13 +0200

    Make name of variable more explicit

--- 2025-08-02_monobloc/src/main.rs
+++ 2025-08-02_monobloc/src/main.rs
@@ -5,17 +5,17 @@ use anyhow::Context;
 fn main() -> anyhow::Result<()> {
     let file = "add.mbl";
 
-    let mut code = String::new();
+    let mut input_code = String::new();
     File::open(file)
         .with_context(|| format!("Opening `{file}`"))?
-        .read_to_string(&mut code)
+        .read_to_string(&mut input_code)
         .with_context(|| format!("Reading code from `{file}`"))?;
 
-    println!("{code}");
+    println!("{input_code}");
 
     let mut stack = Vec::<i32>::new();
 
-    for identifier in code.split_whitespace() {
+    for identifier in input_code.split_whitespace() {
         if let Ok(value) = identifier.parse() {
             stack.push(value);
         } else {
