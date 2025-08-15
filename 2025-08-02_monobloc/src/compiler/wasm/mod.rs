60618c4141a0847e49fe17c3e1da35836913924f
Hanno Braun <hello@hannobraun.com>
2025-08-15 15:19:58 +0200

    Simplify return type

--- 2025-08-02_monobloc/src/compiler/wasm/mod.rs
+++ 2025-08-02_monobloc/src/compiler/wasm/mod.rs
@@ -9,7 +9,7 @@ pub fn compile_module() -> anyhow::Result<Vec<u8>> {
 
     emit_magic(&mut output);
     emit_version(&mut output);
-    emit_type_section(&mut output)?;
+    emit_type_section(&mut output);
     emit_function_section(&mut output)?;
 
     Ok(output)
@@ -23,15 +23,13 @@ fn emit_version(output: &mut Vec<u8>) {
     output.extend([1, 0, 0, 0]);
 }
 
-fn emit_type_section(output: &mut Vec<u8>) -> anyhow::Result<()> {
+fn emit_type_section(output: &mut Vec<u8>) {
     let id = 1;
 
     let mut contents = Vec::new();
     emit_vec(&[], &mut contents);
 
     emit_section(id, contents, output);
-
-    Ok(())
 }
 
 fn emit_function_section(output: &mut Vec<u8>) -> anyhow::Result<()> {
