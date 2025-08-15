38515b904864f14d297c8dd49d69ffde8c5d1e22
Hanno Braun <hello@hannobraun.com>
2025-08-15 19:03:29 +0200

    Prepare to emit type

--- 2025-08-02_monobloc/src/compiler/wasm/vec.rs
+++ 2025-08-02_monobloc/src/compiler/wasm/vec.rs
@@ -1,11 +1,6 @@
 use crate::compiler::wasm::{functions::FuncType, leb128};
 
 pub fn emit_vec(items: &[FuncType], output: &mut Vec<u8>) {
-    assert_eq!(
-        items.len(),
-        0,
-        "Only empty vectors are supported right now.",
-    );
     emit_vec_length(items.len(), output);
     for item in items {
         item.emit(output);
