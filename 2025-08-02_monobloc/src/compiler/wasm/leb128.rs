9f3e839c3b6616e5fa3697c5d9d97ea6819b5c54
Hanno Braun <hello@hannobraun.com>
2025-08-15 19:44:01 +0200

    Fix encoding of vector length

--- 2025-08-02_monobloc/src/compiler/wasm/leb128.rs
+++ 2025-08-02_monobloc/src/compiler/wasm/leb128.rs
@@ -6,6 +6,11 @@ pub fn emit_usize(value: usize, output: &mut Vec<u8>) {
     emit_u64(value, output);
 }
 
+pub fn emit_u32(value: u32, output: &mut Vec<u8>) {
+    leb128::write::unsigned(output, value.into())
+        .expect("Writing into a `&mut Vec` should never fail.");
+}
+
 pub fn emit_u64(value: u64, output: &mut Vec<u8>) {
     leb128::write::unsigned(output, value)
         .expect("Writing into a `&mut Vec` should never fail.");
