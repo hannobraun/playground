c9b317f82e9308ee9447206c076468bae0747b1e
Hanno Braun <hello@hannobraun.com>
2025-08-15 19:13:34 +0200

    Prepare for follow-on change

--- 2025-08-02_monobloc/src/compiler/wasm/mod.rs
+++ 2025-08-02_monobloc/src/compiler/wasm/mod.rs
@@ -39,7 +39,7 @@ fn emit_function_section(output: &mut Vec<u8>) {
     let id = 3;
 
     let mut contents = Vec::new();
-    let functions = &[];
+    let functions: &[FuncType] = &[];
     emit_vec(functions, &mut contents);
 
     emit_section(id, contents, output);
