2a90fb6d9c486479db9a19f91a53b49c5abcc8a0
Hanno Braun <hello@hannobraun.com>
2025-08-15 19:54:59 +0200

    Fix encoding of section size

--- 2025-08-02_monobloc/src/compiler/wasm/leb128.rs
+++ 2025-08-02_monobloc/src/compiler/wasm/leb128.rs
@@ -1,12 +1,3 @@
-pub fn emit_usize(value: usize, output: &mut Vec<u8>) {
-    let value = value
-        .try_into()
-        .expect("Can always convert `usize` to `u64`.");
-
-    leb128::write::unsigned(output, value)
-        .expect("Writing into a `&mut Vec` should never fail.");
-}
-
 pub fn emit_u32(value: u32, output: &mut Vec<u8>) {
     leb128::write::unsigned(output, value.into())
         .expect("Writing into a `&mut Vec` should never fail.");
--- 2025-08-02_monobloc/src/compiler/wasm/section.rs
+++ 2025-08-02_monobloc/src/compiler/wasm/section.rs
@@ -11,7 +11,11 @@ fn emit_section_id(id: u8, output: &mut Vec<u8>) {
 }
 
 fn emit_section_size(size: usize, output: &mut Vec<u8>) {
-    leb128::emit_usize(size, output);
+    let Ok(size) = size.try_into() else {
+        panic!("Unsupported section size: `{size}`");
+    };
+
+    leb128::emit_u32(size, output);
 }
 
 fn emit_section_contents(contents: Vec<u8>, output: &mut Vec<u8>) {
