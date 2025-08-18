ebbabdbfdaefd61eb6688989c92e432ee4faf6dd
Hanno Braun <hello@hannobraun.com>
2025-08-18 20:56:15 +0200

    Prepare for follow-on change

--- 2025-08-02_monobloc/src/main.rs
+++ 2025-08-02_monobloc/src/main.rs
@@ -1,10 +1,10 @@
+use crate::tests::compile;
+
 mod compiler;
 mod runtime;
 mod tests;
 
 fn main() -> anyhow::Result<()> {
-    use crate::tests::compile;
-
     let stack = compile("examples/single-number.mbl")?;
     assert_eq!(stack, vec![1]);
 
