decd93aee64d04bdf3640c766e84403776219ef9
Hanno Braun <hello@hannobraun.com>
2025-08-18 15:16:38 +0200

    Update name of function

--- 2025-08-02_monobloc/src/compiler/wasm/func_type.rs
+++ 2025-08-02_monobloc/src/compiler/wasm/func_type.rs
@@ -5,12 +5,12 @@ pub struct FuncType {}
 impl Emit for FuncType {
     fn emit(&self, output: &mut Vec<u8>) {
         output.push(0x60);
-        emit_empty_result_type(output);
-        emit_empty_result_type(output);
+        emit_result_type(output);
+        emit_result_type(output);
     }
 }
 
-fn emit_empty_result_type(output: &mut Vec<u8>) {
+fn emit_result_type(output: &mut Vec<u8>) {
     let result_types: &[ValType] = &[];
     emit_vec(result_types, output);
 }
