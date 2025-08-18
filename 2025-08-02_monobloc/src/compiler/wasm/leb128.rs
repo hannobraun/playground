5426044bfceb53b473c3fe676b785f1a508642c3
Hanno Braun <hello@hannobraun.com>
2025-08-18 14:26:54 +0200

    Prepare for follow-on change

--- 2025-08-02_monobloc/src/compiler/wasm/leb128.rs
+++ 2025-08-02_monobloc/src/compiler/wasm/leb128.rs
@@ -6,11 +6,12 @@ pub enum Leb128 {
 
 impl Emit for Leb128 {
     fn emit(&self, output: &mut Vec<u8>) {
-        match *self {
+        let result = match *self {
             Leb128::U32 { value } => {
                 leb128::write::unsigned(output, value.into())
-                    .expect("Writing into a `&mut Vec` should never fail.");
             }
-        }
+        };
+
+        result.expect("Writing into a `&mut Vec` should never fail.");
     }
 }
