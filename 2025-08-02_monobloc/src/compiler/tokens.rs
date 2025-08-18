594ad74d18b90f7893351ea86caf3ef3d86a70aa
Hanno Braun <hello@hannobraun.com>
2025-08-18 21:12:55 +0200

    Add `Tokenizer::process_all_tokens`

--- 2025-08-02_monobloc/src/compiler/tokens.rs
+++ 2025-08-02_monobloc/src/compiler/tokens.rs
@@ -7,32 +7,7 @@ pub fn tokenize(input_code: &str) -> Vec<Token> {
         tokens: Vec::new(),
     };
 
-    while let Some(ch) = tokenizer.chars.next() {
-        if ch.is_whitespace() {
-            if let Ok(value) = tokenizer.current_token.parse() {
-                tokenizer.tokens.push(Token::Number { value });
-            } else {
-                tokenizer.tokens.push(Token::Identifier {
-                    name: mem::take(&mut tokenizer.current_token),
-                });
-            }
-        } else if ch == '#' {
-            while let Some(&ch) = tokenizer.chars.peek() {
-                // This would be redundant, if we handled multiple subsequent
-                // whitespace characters correctly.
-                tokenizer.chars.next();
-
-                if ch == '\n' {
-                    break;
-                } else {
-                    tokenizer.chars.next();
-                    continue;
-                }
-            }
-        } else {
-            tokenizer.current_token.push(ch);
-        }
-    }
+    tokenizer.process_all_tokens();
 
     tokenizer.tokens
 }
@@ -41,6 +16,37 @@ pub struct Tokenizer<'a> {
     pub chars: Peekable<Chars<'a>>,
     pub current_token: String,
     pub tokens: Vec<Token>,
+}
+
+impl Tokenizer<'_> {
+    pub fn process_all_tokens(&mut self) {
+        while let Some(ch) = self.chars.next() {
+            if ch.is_whitespace() {
+                if let Ok(value) = self.current_token.parse() {
+                    self.tokens.push(Token::Number { value });
+                } else {
+                    self.tokens.push(Token::Identifier {
+                        name: mem::take(&mut self.current_token),
+                    });
+                }
+            } else if ch == '#' {
+                while let Some(&ch) = self.chars.peek() {
+                    // This would be redundant, if we handled multiple subsequent
+                    // whitespace characters correctly.
+                    self.chars.next();
+
+                    if ch == '\n' {
+                        break;
+                    } else {
+                        self.chars.next();
+                        continue;
+                    }
+                }
+            } else {
+                self.current_token.push(ch);
+            }
+        }
+    }
 }
 
 pub enum Token {
