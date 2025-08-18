f3f02542a39508c07e23e096ee10ad2fb0522d53
Hanno Braun <hello@hannobraun.com>
2025-08-18 21:07:09 +0200

    Inline redundant variable

--- 2025-08-02_monobloc/src/compiler/tokens.rs
+++ 2025-08-02_monobloc/src/compiler/tokens.rs
@@ -1,12 +1,11 @@
 use std::{iter::Peekable, mem, str::Chars};
 
 pub fn tokenize(input_code: &str) -> Vec<Token> {
-    let chars = input_code.chars().peekable();
     let current_token = String::default();
     let tokens = Vec::new();
 
     let mut tokenizer = Tokenizer {
-        chars,
+        chars: input_code.chars().peekable(),
         current_token,
         tokens,
     };
