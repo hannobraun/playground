131dd3e94189a783100400ba10c1c06025526f46
Hanno Braun <hello@hannobraun.com>
2025-08-18 20:56:25 +0200

    Convert test into an actual test

--- 2025-08-02_monobloc/src/main.rs
+++ 2025-08-02_monobloc/src/main.rs
@@ -1,12 +1,20 @@
+#[cfg(test)]
 use crate::tests::compile;
 
+#[cfg(test)]
 mod compiler;
+#[cfg(test)]
 mod runtime;
+#[cfg(test)]
 mod tests;
 
 fn main() -> anyhow::Result<()> {
+    Ok(())
+}
+
+#[test]
+fn single_number() -> anyhow::Result<()> {
     let stack = compile("examples/single-number.mbl")?;
     assert_eq!(stack, vec![1]);
-
     Ok(())
 }
