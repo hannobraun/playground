647924d7b00da121ab608d23365c82cd14950354
Hanno Braun <hello@hannobraun.com>
2025-08-18 17:46:02 +0200

    Prepare for follow-on change

--- 2025-08-02_monobloc/src/compiler/tokens.rs
+++ 2025-08-02_monobloc/src/compiler/tokens.rs
@@ -2,7 +2,8 @@ pub fn tokenize(input_code: &str) -> Vec<String> {
     let mut tokens = Vec::new();
 
     for token in input_code.split_whitespace() {
-        tokens.push(token.to_string());
+        let token = token.to_string();
+        tokens.push(token);
     }
 
     tokens
