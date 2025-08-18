a8e275fb0998780ca9b12ae405aae237dda81e4c
Hanno Braun <hello@hannobraun.com>
2025-08-18 15:38:55 +0200

    Add `Expression`

--- 2025-08-02_monobloc/src/compiler/wasm/expressions.rs
+++ 2025-08-02_monobloc/src/compiler/wasm/expressions.rs
@@ -1,16 +1,23 @@
-use crate::compiler::wasm::{Emit, instruction::Instruction};
+use crate::compiler::{
+    ir,
+    wasm::{Emit, instruction::Instruction},
+};
 
 pub struct Expressions;
 
 impl Emit for Expressions {
     fn emit(&self, output: &mut Vec<u8>) {
-        compile_expression(output);
+        compile_expression(&ir::Expression::Value { value: 0 }, output);
         End.emit(output);
     }
 }
 
-fn compile_expression(output: &mut Vec<u8>) {
-    Instruction::ConstI32 { value: 0 }.emit(output);
+fn compile_expression(expression: &ir::Expression, output: &mut Vec<u8>) {
+    match *expression {
+        ir::Expression::Value { value } => {
+            Instruction::ConstI32 { value }.emit(output);
+        }
+    }
 }
 
 struct End;
