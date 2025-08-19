e582f55e9b5858b5af77947d7e90a9d3e7ace12e
Hanno Braun <hello@hannobraun.com>
2025-08-19 22:41:04 +0200

    Prepare for follow-on change

--- 2025-08-02_monobloc/src/compiler/input_code.rs
+++ 2025-08-02_monobloc/src/compiler/input_code.rs
@@ -1,15 +1,19 @@
-use std::{fs::File, io::Read, iter::Peekable, str::Chars};
+use std::{fs::File, io::Read, iter::Peekable, path::Path, str::Chars};
 
 use anyhow::Context;
 
 pub fn read_input_code<'a>(
-    path: &str,
+    path: impl AsRef<Path>,
     buf: &'a mut String,
 ) -> anyhow::Result<InputCode<'a>> {
+    let path = path.as_ref();
+
     File::open(path)
-        .with_context(|| format!("Opening `{path}`"))?
+        .with_context(|| format!("Opening `{path}`", path = path.display()))?
         .read_to_string(buf)
-        .with_context(|| format!("Reading code from `{path}`"))?;
+        .with_context(|| {
+            format!("Reading code from `{path}`", path = path.display())
+        })?;
 
     Ok(buf.chars().peekable())
 }
