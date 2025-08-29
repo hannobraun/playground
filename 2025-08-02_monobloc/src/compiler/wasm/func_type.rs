376143caab47f81598b151fbe324d2a03857c2f8
Hanno Braun <hello@hannobraun.com>
2025-08-29 12:11:59 +0200

    Inline redundant function

--- 2025-08-02_monobloc/src/compiler/wasm/func_type.rs
+++ 2025-08-02_monobloc/src/compiler/wasm/func_type.rs
@@ -29,9 +29,5 @@ impl Emit for ResultType<'_> {
 }
 
 fn compile_types(types: &ir::Types) -> Vec<ValType<'_>> {
-    types.iter().map(|ty| compile_type(ty)).collect()
-}
-
-fn compile_type(ty: &ir::Type) -> ValType<'_> {
-    ValType { ty }
+    types.iter().map(|ty| ValType { ty }).collect()
 }
