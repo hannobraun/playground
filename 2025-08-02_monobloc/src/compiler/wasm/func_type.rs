6f0736d00f1fc5c18d878c95c677101f7789d4ba
Hanno Braun <hello@hannobraun.com>
2025-08-29 11:44:03 +0200

    Update import

--- 2025-08-02_monobloc/src/compiler/wasm/func_type.rs
+++ 2025-08-02_monobloc/src/compiler/wasm/func_type.rs
@@ -1,5 +1,5 @@
 use crate::compiler::{
-    ir::{Signature, Type, Types},
+    ir::{self, Type, Types},
     wasm::{
         Emit,
         val_type::{NumType, ValType},
@@ -8,12 +8,12 @@ use crate::compiler::{
 };
 
 pub struct FuncType<'a> {
-    pub signature: &'a Signature,
+    pub signature: &'a ir::Signature,
 }
 
 impl Emit for FuncType<'_> {
     fn emit(&self, target: &mut Vec<u8>) {
-        let Signature { inputs, outputs } = self.signature;
+        let ir::Signature { inputs, outputs } = self.signature;
 
         target.push(0x60);
         ResultType { types: inputs }.emit(target);
