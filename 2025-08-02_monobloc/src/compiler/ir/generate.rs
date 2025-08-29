7a5f54b9c764b24fd2d6e9b36428862c3a2472db
Hanno Braun <hello@hannobraun.com>
2025-08-29 11:35:25 +0200

    Move `generate_ir` to dedicated module

---
+++ 2025-08-02_monobloc/src/compiler/ir/generate.rs
@@ -0,0 +1,60 @@
+use crate::compiler::{
+    intrinsics::{Intrinsic, Resolver},
+    ir::{Expression, Function, Stack},
+    syntax::{NodeKind, SyntaxNode},
+    types::{Signature, Type},
+};
+
+pub fn generate_ir(syntax: Vec<SyntaxNode>, resolver: &Resolver) -> Function {
+    let mut stack = Stack {
+        inputs: Vec::new(),
+        outputs: Vec::new(),
+    };
+    let mut body = Vec::new();
+
+    for node in syntax {
+        match node.kind {
+            NodeKind::Binding { names: _ } => {
+                // Not supported yet; ignoring for now.
+            }
+            NodeKind::Comment { text: _ } => {
+                // ignoring comment
+            }
+            NodeKind::Identifier { name } => {
+                if let Some(intrinsic) =
+                    resolver.intrinsics.get(&node.id).copied()
+                {
+                    let [inputs, outputs] = intrinsic.signature();
+
+                    body.push(Expression::Intrinsic { intrinsic });
+
+                    for &input in inputs {
+                        stack.pop(input);
+                    }
+                    for &output in outputs {
+                        stack.push(output);
+                    }
+                } else {
+                    println!("Unknown identifier: `{name}`");
+                    body.push(Expression::Intrinsic {
+                        intrinsic: Intrinsic::Panic,
+                    });
+                }
+            }
+            NodeKind::Integer { value, format: _ } => {
+                body.push(Expression::Intrinsic {
+                    intrinsic: Intrinsic::Integer { value },
+                });
+                stack.push(Type::I32);
+            }
+        }
+    }
+
+    Function {
+        signature: Signature {
+            inputs: stack.inputs,
+            outputs: stack.outputs,
+        },
+        body,
+    }
+}
