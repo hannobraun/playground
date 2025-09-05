a6e57aa573736d1507ad18b37bc0c38a55752a31
Hanno Braun <hello@hannobraun.com>
2025-09-05 14:17:44 +0200

    Prepare to emit functions for all blocks

--- 2025-08-02_monobloc/src/compiler/wasm/function_section.rs
+++ 2025-08-02_monobloc/src/compiler/wasm/function_section.rs
@@ -7,14 +7,22 @@ pub struct FunctionSection;
 impl Emit for FunctionSection {
     fn emit(&self, target: &mut Vec<u8>) {
         let mut contents = Vec::new();
-        WasmVec {
-            items: &[TypeIdx { index: 0 }],
-        }
-        .emit(&mut contents);
+        TypeIdxVec {}.emit(&mut contents);
 
         Section {
             id: 3,
             contents: &contents,
+        }
+        .emit(target);
+    }
+}
+
+struct TypeIdxVec {}
+
+impl Emit for TypeIdxVec {
+    fn emit(&self, target: &mut Vec<u8>) {
+        WasmVec {
+            items: &[TypeIdx { index: 0 }],
         }
         .emit(target);
     }
