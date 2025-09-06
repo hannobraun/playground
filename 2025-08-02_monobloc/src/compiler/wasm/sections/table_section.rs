da95592ee4ecf0d0e8e8b0eb9cd2b004b4eb2922
Hanno Braun <hello@hannobraun.com>
2025-09-06 21:21:16 +0200

    Prepare to populate table section

--- 2025-08-02_monobloc/src/compiler/wasm/sections/table_section.rs
+++ 2025-08-02_monobloc/src/compiler/wasm/sections/table_section.rs
@@ -1,18 +1,27 @@
-use crate::compiler::wasm::{sections::Section, types::TableType, vec::WasmVec, Emit};
+use crate::compiler::wasm::{
+    Emit, sections::Section, types::TableType, vec::WasmVec,
+};
 
 pub struct TableSection {}
 
 impl Emit for TableSection {
     fn emit(&self, target: &mut Vec<u8>) {
         let mut contents = Vec::new();
-
-        let items: &[TableType] = &[];
-        WasmVec { items }.emit(&mut contents);
+        TableTypeVec {}.emit(&mut contents);
 
         Section {
             id: 4,
             contents: &contents,
         }
         .emit(target);
+    }
+}
+
+struct TableTypeVec {}
+
+impl Emit for TableTypeVec {
+    fn emit(&self, target: &mut Vec<u8>) {
+        let items: &[TableType] = &[];
+        WasmVec { items }.emit(target);
     }
 }
