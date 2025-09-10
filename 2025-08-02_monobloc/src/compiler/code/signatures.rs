613ad1700bdd9c0f7beddbbe43becd29dab94fc6
Hanno Braun <hello@hannobraun.com>
2025-09-10 16:44:40 +0200

    Simplify name of struct field

--- 2025-08-02_monobloc/src/compiler/code/signatures.rs
+++ 2025-08-02_monobloc/src/compiler/code/signatures.rs
@@ -3,25 +3,23 @@ use std::collections::BTreeMap;
 use crate::compiler::{code::nodes::NodeId, ir::Signature};
 
 pub struct Signatures {
-    signatures_by_block: BTreeMap<NodeId, Signature>,
+    inner: BTreeMap<NodeId, Signature>,
 }
 
 impl Signatures {
     pub fn insert(&mut self, id: NodeId, signature: Signature) {
-        self.signatures_by_block.insert(id, signature);
+        self.inner.insert(id, signature);
     }
 
     pub fn signature_of_block(&self, id: &NodeId) -> &Signature {
-        self.signatures_by_block
-            .get(id)
-            .expect("Block not available")
+        self.inner.get(id).expect("Block not available")
     }
 }
 
 impl Signatures {
     pub fn new() -> Self {
         Self {
-            signatures_by_block: BTreeMap::new(),
+            inner: BTreeMap::new(),
         }
     }
 }
