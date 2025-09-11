5bc39298b1ff94fdfe77812fe9a1819c0ef51974
Hanno Braun <hello@hannobraun.com>
2025-09-11 15:43:26 +0200

    Update name of module

--- 2025-08-02_monobloc/src/compiler/passes/mod.rs
+++ 2025-08-02_monobloc/src/compiler/passes/mod.rs
@@ -1,9 +1,9 @@
-mod inferrer;
+mod infer;
 mod parser;
 mod resolver;
 mod tokenizer;
 
 pub use self::{
-    inferrer::infer_types, parser::Parser, resolver::Resolver,
+    infer::infer_types, parser::Parser, resolver::Resolver,
     tokenizer::Tokenizer,
 };
