e48c187772e5a310eb195a50084bfb5c095fb897
Hanno Braun <hello@hannobraun.com>
2025-09-17 22:59:17 +0200

    Move spec-related code to dedicated module

---
+++ 2025-09-17_stack-assembly/src/spec.rs
@@ -0,0 +1,64 @@
+use std::{fs::File, io::Read, path::Path};
+
+use anyhow::bail;
+use crossterm::style::{Color, Stylize};
+
+use crate::evaluate::evaluate;
+
+pub fn run_test(spec_dir: &Path, test_file: &Path) -> anyhow::Result<()> {
+    assert!(!test_file.is_dir());
+
+    let mut code = String::new();
+    File::open(test_file)?.read_to_string(&mut code)?;
+
+    use SpecTestOutcome::*;
+
+    let expected_outcome = {
+        let Some(file_name) = test_file.file_name() else {
+            unreachable!(
+                "Path is not a directory, but has no file name either."
+            );
+        };
+        let Some(file_name) = file_name.to_str() else {
+            bail!("Can't represent test file name as UTF-8.");
+        };
+
+        let Some((file_name_without_extension, _)) = file_name.rsplit_once(".")
+        else {
+            bail!("Expecting test file name to have an extension.");
+        };
+        let Some((_, pass_or_fail)) =
+            file_name_without_extension.rsplit_once(".")
+        else {
+            panic!("Expecting test file name to have a pass/fail specifier.");
+        };
+
+        match pass_or_fail {
+            "pass" => Pass,
+            "fail" => Fail,
+
+            unexpected => {
+                bail!("Unexpected pass/fail specifier (`{unexpected}`).");
+            }
+        }
+    };
+
+    match (evaluate(&code), expected_outcome) {
+        (Ok(()), Pass) | (Err(_), Fail) => {
+            print!("{}", "PASS".bold().with(Color::DarkGreen))
+        }
+        (Ok(()), Fail) | (Err(_), Pass) => {
+            print!("{}", "FAIL".bold().with(Color::DarkRed))
+        }
+    }
+
+    let path = test_file.strip_prefix(spec_dir)?;
+    println!(" {path}", path = path.display());
+
+    Ok(())
+}
+
+enum SpecTestOutcome {
+    Pass,
+    Fail,
+}
