8230e6d35b56f55f84bd2c6d5b9f014c767e4afb
Hanno Braun <hello@hannobraun.com>
2025-09-17 21:29:53 +0200

    Ignore directories

--- 2025-09-17_stack-assembly/src/main.rs
+++ 2025-09-17_stack-assembly/src/main.rs
@@ -3,6 +3,11 @@ use walkdir::WalkDir;
 fn main() -> anyhow::Result<()> {
     for entry in WalkDir::new("spec") {
         let entry = entry?;
+
+        if entry.path().is_dir() {
+            continue;
+        }
+
         dbg!(entry);
     }
 
