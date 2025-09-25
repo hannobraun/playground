302e0a52ff74927f3a690253618e5896eda12019
Hanno Braun <hello@hannobraun.com>
2025-09-25 21:31:13 +0200

    Clean up `spec` module to prepare for expanding it

---
+++ archive/2025-10-27_stack-assembly/src/spec/empty_program.rs
@@ -0,0 +1,13 @@
+use crate::Program;
+
+#[test]
+fn empty_program_exits_with_empty_stack() {
+    let program = Program::compile_and_run("");
+    assert_eq!(program.operands(), &vec![]);
+}
+
+#[test]
+fn empty_program_should_not_trigger_effect() {
+    let program = Program::compile_and_run("");
+    assert_eq!(program.effect(), None);
+}
