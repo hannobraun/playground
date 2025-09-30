fbb7a5523f2642c5d8b43faebfd385b76bfeebad
Hanno Braun <hello@hannobraun.com>
2025-09-30 21:22:32 +0200

    Derive `Debug` for `Instruction`
    I needed this to debug something right now.

--- archive/2025-10-27_stack-assembly/src/runtime.rs
+++ archive/2025-10-27_stack-assembly/src/runtime.rs
@@ -75,12 +75,14 @@ pub enum StepOutcome {
     Finished,
 }
 
+#[derive(Debug)]
 pub enum Instruction {
     Operator { operator: Operator },
     Reference { name: String },
     Return,
 }
 
+#[derive(Debug)]
 pub enum Operator {
     Integer { value: i32 },
 
