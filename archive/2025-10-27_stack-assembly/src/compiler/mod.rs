8b5d82c17630f33bd90efd95c84e543202cf69c1
Hanno Braun <hello@hannobraun.com>
2025-10-02 15:35:29 +0200

    Move parsing and translation to dedicated modules
    I'm about to expand the `compiler` module further. This is a step in
    preparation of that, to prevent the module from becoming overloaded.

--- archive/2025-10-27_stack-assembly/src/compiler/mod.rs
+++ archive/2025-10-27_stack-assembly/src/compiler/mod.rs
@@ -1,6 +1,12 @@
+mod parse;
+mod translate;
+
 use crate::{
-    Effect,
-    instructions::{Instruction, Instructions, Labels, Operator},
+    compiler::{
+        parse::{parse_label, parse_operator, parse_reference},
+        translate::{translate_label, translate_operator, translate_reference},
+    },
+    instructions::{Instructions, Labels},
 };
 
 pub fn compile(input: &str) -> (Instructions, Labels) {
@@ -19,84 +25,4 @@ pub fn compile(input: &str) -> (Instructions, Labels) {
     }
 
     (instructions, labels)
-}
-
-fn parse_label(token: &str) -> Option<&str> {
-    if let Some((label, "")) = token.rsplit_once(":") {
-        Some(label)
-    } else {
-        None
-    }
-}
-
-fn parse_operator(token: &str) -> Option<Operator> {
-    let operator = if token == "call" {
-        Operator::Call
-    } else if token == "call_if" {
-        Operator::CallIf
-    } else if token == "drop0" {
-        Operator::Drop0
-    } else if token == "yield" {
-        Operator::Yield
-    } else if let Ok(value) = token.parse() {
-        Operator::Integer { value }
-    } else {
-        return None;
-    };
-
-    Some(operator)
-}
-
-fn parse_reference(token: &str) -> Option<&str> {
-    if let Some(("", reference)) = token.split_once("@") {
-        Some(reference)
-    } else {
-        None
-    }
-}
-
-fn translate_label(
-    name: &str,
-    instructions: &mut Instructions,
-    labels: &mut Labels,
-) {
-    // Encountering a label means that the previous function has ended.
-    instructions.push(Instruction::Return);
-
-    let address = {
-        let address = instructions.len();
-
-        let Ok(address) = address.try_into() else {
-            // This is okay for now, but it would be nicer to reject
-            // this when pushing to `instructions`.
-            panic!(
-                "Label `{name}` points to address `{address}`, which can't be \
-                represented as a signed 32-bit integer. Too much code!"
-            );
-        };
-
-        address
-    };
-
-    // This overwrites any previous label of the same name. Fine for now, but it
-    // would be better if this were an error.
-    labels.insert(name.to_string(), address);
-}
-
-fn translate_operator(
-    operator: Option<Operator>,
-    instructions: &mut Instructions,
-) {
-    if let Some(operator) = operator {
-        instructions.push(Instruction::Operator { operator });
-    } else {
-        instructions.push(Instruction::Trigger {
-            effect: Effect::UnknownOperator,
-        })
-    }
-}
-
-fn translate_reference(name: &str, instructions: &mut Instructions) {
-    let name = name.to_string();
-    instructions.push(Instruction::Reference { name });
 }
---
+++ archive/2025-10-27_stack-assembly/src/compiler/translate.rs
@@ -0,0 +1,50 @@
+use crate::{
+    Effect,
+    instructions::{Instruction, Instructions, Labels, Operator},
+};
+
+pub fn translate_label(
+    name: &str,
+    instructions: &mut Instructions,
+    labels: &mut Labels,
+) {
+    // Encountering a label means that the previous function has ended.
+    instructions.push(Instruction::Return);
+
+    let address = {
+        let address = instructions.len();
+
+        let Ok(address) = address.try_into() else {
+            // This is okay for now, but it would be nicer to reject
+            // this when pushing to `instructions`.
+            panic!(
+                "Label `{name}` points to address `{address}`, which can't be \
+                represented as a signed 32-bit integer. Too much code!"
+            );
+        };
+
+        address
+    };
+
+    // This overwrites any previous label of the same name. Fine for now, but it
+    // would be better if this were an error.
+    labels.insert(name.to_string(), address);
+}
+
+pub fn translate_operator(
+    operator: Option<Operator>,
+    instructions: &mut Instructions,
+) {
+    if let Some(operator) = operator {
+        instructions.push(Instruction::Operator { operator });
+    } else {
+        instructions.push(Instruction::Trigger {
+            effect: Effect::UnknownOperator,
+        })
+    }
+}
+
+pub fn translate_reference(name: &str, instructions: &mut Instructions) {
+    let name = name.to_string();
+    instructions.push(Instruction::Reference { name });
+}
