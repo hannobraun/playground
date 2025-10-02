993b99fba67263cce7088e39cebc5bfa1238daec
Hanno Braun <hello@hannobraun.com>
2025-10-02 21:01:58 +0200

    Simplify script translation code

--- archive/2025-10-27_stack-assembly/src/compiler/mod.rs
+++ archive/2025-10-27_stack-assembly/src/compiler/mod.rs
@@ -16,18 +16,16 @@ pub fn compile(input: &str) -> (Instructions, Labels) {
     let mut instructions = Instructions::new();
     let mut labels = Labels::new();
 
-    let all_functions = [script.root]
-        .map(|function| (None, function))
-        .into_iter()
-        .chain(
-            script
-                .functions
-                .into_iter()
-                .map(|(name, function)| (Some(name), function)),
-        );
+    let name = None;
+    translate_function(name, script.root, &mut instructions, &mut labels);
 
-    for (name, function) in all_functions {
-        translate_function(name, function, &mut instructions, &mut labels);
+    for (name, function) in script.functions {
+        translate_function(
+            Some(name),
+            function,
+            &mut instructions,
+            &mut labels,
+        );
     }
 
     (instructions, labels)
