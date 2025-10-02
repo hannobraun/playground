09d5288d4e6ab9dd4aa539c9f4ef7dd0ddafe147
Hanno Braun <hello@hannobraun.com>
2025-10-02 20:03:58 +0200

    Parse token stream into structured `Script` type
    Again, this only makes the current situation more complicated. But it
    enables a next step, the implementation of tail call optimization, which
    requires this structured per-function representation.

--- archive/2025-10-27_stack-assembly/src/compiler/mod.rs
+++ archive/2025-10-27_stack-assembly/src/compiler/mod.rs
@@ -4,8 +4,8 @@ mod translate;
 
 use crate::{
     compiler::{
-        parse::parse_operator,
-        tokenize::{Token, tokenize},
+        parse::{Expression, parse_script},
+        tokenize::tokenize,
         translate::{translate_label, translate_operator, translate_reference},
     },
     instructions::{Instructions, Labels},
@@ -13,23 +13,36 @@ use crate::{
 
 pub fn compile(input: &str) -> (Instructions, Labels) {
     let tokens = tokenize(input);
+    let script = parse_script(tokens);
 
     let mut instructions = Instructions::new();
     let mut labels = Labels::new();
 
-    for token in tokens {
-        match token {
-            Token::Label { name } => {
-                translate_label(name, &mut instructions, &mut labels);
+    let all_functions = [script.root]
+        .map(|function| (None, function))
+        .into_iter()
+        .chain(
+            script
+                .functions
+                .into_iter()
+                .map(|(name, function)| (Some(name), function)),
+        );
+
+    for (name, function) in all_functions {
+        if let Some(name) = name {
+            translate_label(name, &mut instructions, &mut labels);
+        }
+
+        for expression in function.body {
+            match expression {
+                Expression::Operator { operator } => {
+                    translate_operator(operator, &mut instructions);
+                }
+                Expression::Reference { name } => {
+                    translate_reference(name, &mut instructions);
+                }
             }
-            Token::Operator { name } => {
-                let operator = parse_operator(name);
-                translate_operator(operator, &mut instructions);
-            }
-            Token::Reference { name } => {
-                translate_reference(name, &mut instructions);
-            }
-        };
+        }
     }
 
     (instructions, labels)
--- archive/2025-10-27_stack-assembly/src/compiler/parse.rs
+++ archive/2025-10-27_stack-assembly/src/compiler/parse.rs
@@ -1,4 +1,65 @@
-use crate::instructions::Operator;
+use crate::{compiler::tokenize::Token, instructions::Operator};
+
+pub fn parse_script<'r>(
+    tokens: impl IntoIterator<Item = Token<'r>>,
+) -> Script<'r> {
+    let mut tokens = tokens.into_iter();
+
+    let (root, mut current_label) = Function::parse(&mut tokens);
+    let mut functions = Vec::new();
+
+    while let Some(name) = current_label {
+        let (function, next_label) = Function::parse(&mut tokens);
+        functions.push((name, function));
+        current_label = next_label;
+    }
+
+    Script { root, functions }
+}
+
+pub struct Script<'r> {
+    pub root: Function<'r>,
+    pub functions: Vec<(&'r str, Function<'r>)>,
+}
+
+pub struct Function<'r> {
+    pub body: Vec<Expression<'r>>,
+}
+
+impl<'r> Function<'r> {
+    fn parse(
+        tokens: &mut impl Iterator<Item = Token<'r>>,
+    ) -> (Self, Option<&'r str>) {
+        let mut function = Self { body: Vec::new() };
+
+        loop {
+            let Some(token) = tokens.next() else {
+                // No more tokens, which means this is the last function.
+                return (function, None);
+            };
+
+            let expression = match token {
+                Token::Label { name } => {
+                    // A label starts the next function. We're done here.
+                    return (function, Some(name));
+                }
+
+                Token::Operator { name } => {
+                    let operator = parse_operator(name);
+                    Expression::Operator { operator }
+                }
+                Token::Reference { name } => Expression::Reference { name },
+            };
+
+            function.body.push(expression);
+        }
+    }
+}
+
+pub enum Expression<'r> {
+    Operator { operator: Option<Operator> },
+    Reference { name: &'r str },
+}
 
 pub fn parse_operator(token: &str) -> Option<Operator> {
     let operator = if token == "call" {
