2a73e2485163edba1fb5686202a3dd2549f3339d
Hanno Braun <hello@hannobraun.com>
2025-10-09 21:56:52 +0200

    Update name of test
    The new name allows for more tests of `drop` family functions to be
    added, while keeping their names simple and consistent.

--- archive/2025-10-27_stack-assembly/src/spec/operands.rs
+++ archive/2025-10-27_stack-assembly/src/spec/operands.rs
@@ -1,7 +1,7 @@
 use crate::{Effect, Program};
 
 #[test]
-fn drop0_drops_top_operand() {
+fn drop0_drops_operand_at_index_0() {
     let program = Program::compile_and_run("3 drop0");
     assert_eq!(program.operands(), &vec![]);
     assert_eq!(program.effect(), None);
