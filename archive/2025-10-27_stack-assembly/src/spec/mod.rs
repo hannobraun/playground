f882c3efea5a660855dfdc058e5e31022ea4dd2d
Hanno Braun <hello@hannobraun.com>
2025-10-10 20:40:25 +0200

    Remove errant word from name of test

--- archive/2025-10-27_stack-assembly/src/spec/mod.rs
+++ archive/2025-10-27_stack-assembly/src/spec/mod.rs
@@ -18,7 +18,7 @@ fn evaluate_integers() {
 }
 
 #[test]
-fn unknown_operator_should_stop_trigger_effect() {
+fn unknown_operator_should_trigger_effect() {
     let mut program = Program::compile_and_run("3 unknown_operator 5");
     assert_eq!(program.operands(), &vec![3]);
     assert_eq!(program.effect(), Some(&Effect::UnknownOperator));
