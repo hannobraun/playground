fa0917b333ce58ab0cc7eb5a5fba01e8f5427aef
Hanno Braun <hello@hannobraun.com>
2025-10-10 21:24:04 +0200

    Merge related tests

--- archive/2025-10-27_stack-assembly/src/spec/mod.rs
+++ archive/2025-10-27_stack-assembly/src/spec/mod.rs
@@ -7,13 +7,10 @@ mod memory;
 mod operands;
 
 #[test]
-fn stack_stays_empty_if_program_never_starts() {
-    let mut program = Program::compile("3");
-    assert_eq!(program.operands(), &vec![]);
-}
-
-#[test]
 fn evaluate_integers() {
-    let mut program = Program::compile_and_run("3 5");
+    let mut program = Program::compile("3 5");
+    assert_eq!(program.operands(), &vec![]);
+
+    program.continue_();
     assert_eq!(program.operands(), &vec![3, 5]);
 }
