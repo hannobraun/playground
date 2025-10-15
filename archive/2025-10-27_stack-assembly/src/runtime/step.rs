7e84a4e8db9d92d74e9b22d25893f7b4970a9bdb
Hanno Braun <hello@hannobraun.com>
2025-10-15 15:48:57 +0200

    Make name of variable more explicit

--- archive/2025-10-27_stack-assembly/src/runtime/step.rs
+++ archive/2025-10-27_stack-assembly/src/runtime/step.rs
@@ -25,9 +25,9 @@ pub fn step(
             let b = operands.pop()?;
             let a = operands.pop()?;
 
-            let value = i32::wrapping_add(a.inner, b.inner);
+            let output = i32::wrapping_add(a.inner, b.inner);
 
-            operands.push(Value { inner: value });
+            operands.push(Value { inner: output });
         }
         Instruction::And => {
             let b = operands.pop()?;
