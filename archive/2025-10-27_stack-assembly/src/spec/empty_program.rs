feac76a7fae583857be92b45ca15d24ec706edae
Hanno Braun <hello@hannobraun.com>
2025-10-15 19:49:11 +0200

    Update names of tests

--- archive/2025-10-27_stack-assembly/src/spec/empty_program.rs
+++ archive/2025-10-27_stack-assembly/src/spec/empty_program.rs
@@ -1,13 +1,13 @@
 use crate::Application;
 
 #[test]
-fn empty_program_exits_with_empty_stack() {
+fn empty_application_exits_with_empty_stack() {
     let mut app = Application::compile_and_run("");
     assert_eq!(app.operands(), &vec![]);
 }
 
 #[test]
-fn empty_program_should_not_trigger_effect() {
+fn empty_application_should_not_trigger_effect() {
     let app = Application::compile_and_run("");
     assert_eq!(app.effect(), None);
 }
