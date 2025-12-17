96d4527b55adf75a934b46840a8c4cdc365926b5
Hanno Braun <hello@hannobraun.com>
2025-12-17 15:19:39 +0100

    Light on-board LEDs

--- 2025-12-17_ergot-demo/src/main.rs
+++ 2025-12-17_ergot-demo/src/main.rs
@@ -2,8 +2,32 @@
 #![no_main]
 
 use embassy_executor::Spawner;
-use embassy_rp as _;
+use embassy_rp::{
+    bind_interrupts,
+    peripherals::PIO0,
+    pio::{InterruptHandler, Pio},
+    pio_programs::ws2812::{PioWs2812, PioWs2812Program},
+};
 use panic_halt as _;
+use smart_leds::RGB8;
+
+bind_interrupts!(struct Irqs {
+    PIO0_IRQ_0 => InterruptHandler<PIO0>;
+});
 
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) {}
+async fn main(_spawner: Spawner) {
+    let p = embassy_rp::init(Default::default());
+
+    let Pio {
+        mut common, sm0, ..
+    } = Pio::new(p.PIO0, Irqs);
+
+    let program = PioWs2812Program::new(&mut common);
+    let mut ws2812 =
+        PioWs2812::new(&mut common, sm0, p.DMA_CH0, p.PIN_23, &program);
+
+    ws2812
+        .write(&[RGB8::new(255, 0, 0), RGB8::new(0, 255, 0)])
+        .await;
+}
