478d5d82e8a3f2298044d315ff11b61495c70069
Hanno Braun <hello@hannobraun.com>
2025-12-20 15:34:52 +0100

    Send request to an endpoint
    The firmware doesn't handle this yet, so it quits with a "no route"
    error.

--- 2025-12-17_ergot-demo/cli/src/main.rs
+++ 2025-12-17_ergot-demo/cli/src/main.rs
@@ -1,5 +1,26 @@
+use anyhow::anyhow;
+use ergot::{Address, endpoint, toolkits::nusb_v0_1::RouterStack};
+
+endpoint! {
+    LedEndpoint,
+    [[u8; 3]; 2], // request: two colors
+    (),           // response
+    "led"
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    println!("Hello, world!");
+    let net_stack = RouterStack::new();
+
+    let () = net_stack
+        .endpoints()
+        .request::<LedEndpoint>(
+            Address::unknown(),
+            &[[255, 0, 0], [0, 255, 0]],
+            None,
+        )
+        .await
+        .map_err(|err| anyhow!("{err:?}"))?;
+
     Ok(())
 }
