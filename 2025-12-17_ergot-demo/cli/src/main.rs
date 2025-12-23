9d25db91cceb02ac5244c1762d29fc8210a5972a
Hanno Braun <hello@hannobraun.com>
2025-12-23 20:32:46 +0100

    Retry setting LED color in a loop

--- 2025-12-17_ergot-demo/cli/src/main.rs
+++ 2025-12-17_ergot-demo/cli/src/main.rs
@@ -1,21 +1,37 @@
+use std::time::Duration;
+
 use anyhow::anyhow;
 use ergot::{Address, toolkits::nusb_v0_1::RouterStack};
 
 use shared::LedEndpoint;
+use tokio::time::sleep;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let net_stack = RouterStack::new();
 
-    let () = net_stack
-        .endpoints()
-        .request::<LedEndpoint>(
-            Address::unknown(),
-            &[[255, 0, 0], [0, 255, 0]],
-            None,
-        )
-        .await
-        .map_err(|err| anyhow!("Failed to set LED color: {err:?}"))?;
+    loop {
+        let result = net_stack
+            .endpoints()
+            .request::<LedEndpoint>(
+                Address::unknown(),
+                &[[255, 0, 0], [0, 255, 0]],
+                None,
+            )
+            .await
+            .map_err(|err| anyhow!("Failed to set LED color: {err:?}"));
+
+        match result {
+            Ok(()) => {
+                break;
+            }
+            Err(err) => {
+                eprintln!("{err:?}");
+                sleep(Duration::from_millis(500)).await;
+                continue;
+            }
+        }
+    }
 
     Ok(())
 }
