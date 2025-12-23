43f92175b1ecbc648ba1a7b0923da79ef56d9863
Hanno Braun <hello@hannobraun.com>
2025-12-23 20:34:02 +0100

    Search for devices
    According to the `ergot-nusb-router` demo, this seems to be missing, but
    it alone is not enough. I'm still getting "no route" errors.
    
    Also, I'm only seeing the "found new device" message, if I run this as
    root.

--- 2025-12-17_ergot-demo/cli/src/main.rs
+++ 2025-12-17_ergot-demo/cli/src/main.rs
@@ -1,16 +1,37 @@
-use std::time::Duration;
+use std::{collections::HashSet, time::Duration};
 
 use anyhow::anyhow;
-use ergot::{Address, toolkits::nusb_v0_1::RouterStack};
+use ergot::{
+    Address,
+    toolkits::nusb_v0_1::{
+        RouterStack, find_new_devices, register_router_interface,
+    },
+};
 
 use shared::LedEndpoint;
 use tokio::time::sleep;
+
+const MAX_ERGOT_BUFFER_SIZE: u16 = 1024;
+const OUTGOING_BUFFER_SIZE: usize = 4096;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let net_stack = RouterStack::new();
 
     loop {
+        for device in find_new_devices(&HashSet::new()).await {
+            eprintln!("Found new device.");
+
+            register_router_interface(
+                &net_stack,
+                device,
+                MAX_ERGOT_BUFFER_SIZE,
+                OUTGOING_BUFFER_SIZE,
+            )
+            .await
+            .map_err(|err| anyhow!("{err:?}"))?;
+        }
+
         let result = net_stack
             .endpoints()
             .request::<LedEndpoint>(
