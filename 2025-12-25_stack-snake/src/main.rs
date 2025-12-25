10dc65c88141c8a51fae6155abc4d19f39b043b6
Hanno Braun <hello@hannobraun.com>
2025-12-25 15:13:25 +0100

    Evaluate script

--- 2025-12-25_stack-snake/src/main.rs
+++ 2025-12-25_stack-snake/src/main.rs
@@ -1,10 +1,15 @@
 use std::{fs::File, io::Read};
 
+use stack_assembly::Eval;
+
 fn main() -> anyhow::Result<()> {
     let mut script = String::new();
     File::open("snake.stack")?.read_to_string(&mut script)?;
 
-    dbg!(script);
+    let mut eval = Eval::start(&script);
+
+    let effect = eval.run();
+    eprintln!("Script triggered effect: {effect:?}");
 
     Ok(())
 }
