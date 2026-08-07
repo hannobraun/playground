fbaf23aa245e4d1b36782b566249b4ab933d1388
Hanno Braun <hello@hannobraun.com>
2026-08-07 13:04:42 +0200

    Implement CI build in a local build tool
    This allows developers to run the complete build locally, with the CI
    pipeline reusing the same code.

---
+++ builder/src/main.rs
@@ -0,0 +1,29 @@
+use std::process::Command;
+
+use anyhow::bail;
+
+fn main() -> anyhow::Result<()> {
+    run("Check formatting", "cargo fmt --check")?;
+    // Without `--all-targets`, Clippy will not check code gated by
+    // `#[cfg(test)]`.
+    run("Run Clippy", "cargo clippy --all-targets")?;
+    run("Run test suite", "cargo test")?;
+    run("Build documentation", "cargo doc")?;
+
+    Ok(())
+}
+
+fn run(description: &str, command: &str) -> anyhow::Result<()> {
+    println!();
+    println!();
+    println!("=== {description}");
+    println!();
+
+    let status = Command::new("sh").args(["-c", command]).status()?;
+
+    if !status.success() {
+        bail!("Command `{command}` failed with status `{status:?}`");
+    }
+
+    Ok(())
+}
