00edf6d306ed024a339c66801ced3f7fd1f61234
Hanno Braun <hello@hannobraun.com>
2026-08-07 13:02:44 +0200

    Add `shell.nix`
    This specificies the operating-system-level build dependencies, making
    it easier to install those for local development.
    
    Currently, this duplicates the dependencies already specified in the CI
    build pipeline. I'm going to address that later in this pull request.

---
+++ shell.nix
@@ -0,0 +1,12 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+pkgs.mkShell {
+  packages = with pkgs; [
+    # This project is written in Rust, and Rustup is the best way to get a Rust
+    # toolchain.
+    rustup
+
+    # Rust uses `cc` as a linker, which this package provides.
+    clang
+  ];
+}
