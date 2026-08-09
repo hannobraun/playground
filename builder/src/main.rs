88c1b32bc607b6934833165cc72936d5fbf333a7
Hanno Braun <hello@hannobraun.com>
2026-08-09 10:57:57 +0200

    builder: Add explicit `build` CLI command

--- builder/src/main.rs
+++ builder/src/main.rs
@@ -1,7 +1,27 @@
+use clap::Parser;
+
 mod build;
 mod util;
 
 fn main() -> anyhow::Result<()> {
-    build::build()?;
+    let args = Args::parse();
+
+    match args.command {
+        Some(Command::Build) | None => {
+            build::build()?;
+        }
+    }
+
     Ok(())
+}
+
+#[derive(clap::Parser)]
+struct Args {
+    #[command(subcommand)]
+    command: Option<Command>,
+}
+
+#[derive(clap::Subcommand)]
+enum Command {
+    Build,
 }
