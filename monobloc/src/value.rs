e88792267f90242ac6cc567b4fd60288ca5d4aa2
Hanno Braun <hello@hannobraun.com>
2026-08-14 11:08:07 +0200

    Add conversions from 32-bit types to `Value`
    This is going to come in handy, as we do more with `Value`. Right now,
    it replaces a capability that I have in my internal prototype, which
    eases the transition of the prototype code to the new code in here.

--- monobloc/src/value.rs
+++ monobloc/src/value.rs
@@ -7,3 +7,17 @@ pub struct Value {
     /// The value
     pub bits: u32,
 }
+
+impl From<f32> for Value {
+    fn from(value: f32) -> Self {
+        Value {
+            bits: value.to_bits(),
+        }
+    }
+}
+
+impl From<u32> for Value {
+    fn from(bits: u32) -> Self {
+        Value { bits }
+    }
+}
